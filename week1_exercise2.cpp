8a1b562b5edb7719785321f18907125a272a36c1
Congge Xu <146875584+cgxxcg@users.noreply.github.com>
2024-10-31 13:24:27 -0700

    Finish week1_exercise2.cpp

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -3,10 +3,23 @@
 //http://195.130.59.221/
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
+AnalogIn potentiometer(p15); 
+DigitalOut led(LED1);
 
 int main() {
     while (1) {
         printf("Week 1 Exercise 2");
+            
+    
+        float potValue = potentiometer.read();
+        int onTime = potValue * 2000;         
+        int offTime = 2000 - onTime;            
+        
+        // Flicker the LED
+        led = 1;                           
+        wait_ms(onTime);                        
+        led = 0;                           
+        wait_ms(offTime);                       
 
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
         wait_ms(500); 
